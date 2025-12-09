6f3c45936c9f8487ab65e43979ed1cb33c844abc
Luis Cupul 04 <186883140+CUPUL-MIU-04@users.noreply.github.com>
2025-12-08 20:35:53 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java")
     id("antlr")
-    id("maven-publish")
 }
 
 allprojects {
@@ -41,65 +40,12 @@ subprojects {
     }
 }
 
-// Configuración de publicación SOLO para subproyectos que no tienen su propia configuración
-configure(subprojects.filter { 
-    // Lista de proyectos que NO tienen configuración de publicación propia
-    // Ajusta según necesites
-    it.name !in listOf("baksmali", "dexlib2", "util", "smali")
-}) {
-    publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/CUPUL-MIU-04/smali")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user")?.toString()
-                    password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token")?.toString()
-                }
-            }
-        }
-        
-        publications {
-            create<MavenPublication>("gpr") {
-                from(components["java"])
-                artifactId = project.name
-                
-                pom {
-                    name.set(project.name)
-                    description.set(project.description ?: "Smali/Baksmali tools")
-                    url.set("https://github.com/CUPUL-MIU-04/smali")
-                    
-                    licenses {
-                        license {
-                            name.set("BSD 2-Clause License")
-                            url.set("https://opensource.org/licenses/BSD-2-Clause")
-                        }
-                    }
-                    
-                    developers {
-                        developer {
-                            id.set("CUPUL-MIU-04")
-                            name.set("CUPUL-MIU-04 Team")
-                        }
-                    }
-                    
-                    scm {
-                        connection.set("scm:git:git://github.com/CUPUL-MIU-04/smali.git")
-                        developerConnection.set("scm:git:ssh://github.com:CUPUL-MIU-04/smali.git")
-                        url.set("https://github.com/CUPUL-MIU-04/smali")
-                    }
-                }
-            }
-        }
-    }
-}
-
 // Tarea para publicar todos los módulos
 tasks.register("publishAllToGitHubPackages") {
     dependsOn(
-        ":dexlib2:publish",
-        ":util:publish",
-        ":smali:publish", 
-        ":baksmali:publish"
+        ":dexlib2:publishGprPublicationToGitHubPackagesRepository",
+        ":util:publishGprPublicationToGitHubPackagesRepository",
+        ":smali:publishGprPublicationToGitHubPackagesRepository",
+        ":baksmali:publishGprPublicationToGitHubPackagesRepository"
     )
 }
