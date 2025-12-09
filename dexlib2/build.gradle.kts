c8518aa9f4f3345f70670439b7e21fd5d02daee9
Luis Cupul 04 <186883140+CUPUL-MIU-04@users.noreply.github.com>
2025-12-08 21:13:57 -0600

    Update build.gradle.kts

--- dexlib2/build.gradle.kts
+++ dexlib2/build.gradle.kts
@@ -1,3 +1,9 @@
+// AGREGA estos plugins al inicio:
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
 dependencies {
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-util:9.5")
@@ -6,5 +12,52 @@ dependencies {
     implementation("org.ow2.asm:asm-analysis:9.5")
     
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.4.0")  // Agrega esta línea
+    testImplementation("org.mockito:mockito-core:5.4.0")
+}
+
+// AGREGA TODO ESTE BLOQUE publishing:
+publishing {
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+            artifactId = "dexlib2"
+            
+            pom {
+                name.set("Dexlib2")
+                description.set("A library for reading/writing Android dex files")
+                url.set("https://github.com/CUPUL-MIU-04/smali")
+                
+                licenses {
+                    license {
+                        name.set("BSD 2-Clause License")
+                        url.set("https://opensource.org/licenses/BSD-2-Clause")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("CUPUL-MIU-04")
+                        name.set("CUPUL-MIU-04 Team")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:git://github.com/CUPUL-MIU-04/smali.git")
+                    developerConnection.set("scm:git:ssh://github.com:CUPUL-MIU-04/smali.git")
+                    url.set("https://github.com/CUPUL-MIU-04/smali")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/CUPUL-MIU-04/smali")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user")?.toString()
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token")?.toString()
+            }
+        }
+    }
 }
