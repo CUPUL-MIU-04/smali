bd172fbd42a2958649ca53e23b962d180f905bd9
Luis Cupul 04 <186883140+CUPUL-MIU-04@users.noreply.github.com>
2025-12-08 21:15:25 -0600

    Update build.gradle.kts

--- baksmali/build.gradle.kts
+++ baksmali/build.gradle.kts
@@ -79,3 +79,16 @@ publishing {
         }
     }
 }
+    
+    // AGREGA ESTE BLOQUE repositories:
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/CUPUL-MIU-04/smali")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user")?.toString()
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token")?.toString()
+            }
+        }
+    }
+}
